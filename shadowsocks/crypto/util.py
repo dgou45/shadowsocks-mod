7fe45961dfa64dc21bdf62ccd8a3ca47667b7abf
dgou45 <46524291+dgou45@users.noreply.github.com>
2023-03-31 10:28:45 +0800

    Update util.py

--- shadowsocks/crypto/util.py
+++ shadowsocks/crypto/util.py
@@ -69,7 +69,7 @@ def find_library(possible_lib_names, search_symbol, library_name,
     lib_names = []
     for lib_name in possible_lib_names:
         lib_names.append(lib_name)
-        lib_names.append('lib' + lib_name)
+        lib_names.append(lib_name)
 
     for name in lib_names:
         if os.name == "nt":
